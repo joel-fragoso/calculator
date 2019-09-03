c4821338c88fbe4683a1688a75c8d71b8ec599da
Joel Fragoso <joel.fragoso@hotmail.com>
2019-09-03 17:08:17 -0300

    updated AJAX files

--- application/Views/home/index.php
+++ application/Views/home/index.php
@@ -2,15 +2,98 @@
 <html lang="pt-br">
     <head>
         <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta http-equiv="x-ua-compatible" content="ie=edge, chrome=1">
+        <link rel="stylesheet" href="assets/css/application.css">
         <title>Calculator</title>
     </head>
     <body>
 
-        <h3>Olá mundo!</h3>
+        <form class="calculator">
 
-        <form>
+            <div class="calculator-head">
+                <div class="row">
+                    <div class="col-12">
+                        <input class="display" type="text" name="display" value="0" readonly>
+                    </div>
+                </div>
+            </div>
 
-            <button class="btn" type="button" name="button" value="Ajax">Ajax</button>
+            <div class="calculator-body">
+
+                <div class="row">
+                    <div class="col-3">
+                        <button class="btn-clear gray" type="button" name="button" value="AC">AC</button>
+                    </div>
+                    <div class="col-3">
+                        <button class="btn gray" type="button" name="button" value="+/-">+/-</button>
+                    </div>
+                    <div class="col-3">
+                        <button class="btn gray" type="button" name="button" value="&#37;">&#37;</button>
+                    </div>
+                    <div class="col-3">
+                        <button class="btn orange" type="button" name="button" value="&divide;">&divide;</button>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-3">
+                        <button class="btn" type="button" name="button" value="7">7</button>
+                    </div>
+                    <div class="col-3">
+                        <button class="btn" type="button" name="button" value="8">8</button>
+                    </div>
+                    <div class="col-3">
+                        <button class="btn" type="button" name="button" value="9">9</button>
+                    </div>
+                    <div class="col-3">
+                        <button class="btn orange" type="button" name="button" value="&times;">&times;</button>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-3">
+                        <button class="btn" type="button" name="button" value="4">4</button>
+                    </div>
+                    <div class="col-3">
+                        <button class="btn" type="button" name="button" value="5">5</button>
+                    </div>
+                    <div class="col-3">
+                        <button class="btn" type="button" name="button" value="6">6</button>
+                    </div>
+                    <div class="col-3">
+                        <button class="btn orange" type="button" name="button" value="&#43;">&#43;</button>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-3">
+                        <button class="btn" type="button" name="button" value="1">1</button>
+                    </div>
+                    <div class="col-3">
+                        <button class="btn" type="button" name="button" value="2">2</button>
+                    </div>
+                    <div class="col-3">
+                        <button class="btn" type="button" name="button" value="3">3</button>
+                    </div>
+                    <div class="col-3">
+                        <button class="btn orange" type="button" name="button" value="&#45;">&#45;</button>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-6">
+                        <button class="btn" type="button" name="button" value="0">0</button>
+                    </div>
+                    <div class="col-3">
+                        <button class="btn" type="button" name="button" value=",">,</button>
+                    </div>
+                    <div class="col-3">
+                        <button class="btn-equal orange" type="button" name="button" value="=">=</button>
+                    </div>
+                </div>
+
+            </div>
 
         </form>
 
--- assets/js/application.js
+++ assets/js/application.js
@@ -1,18 +1,50 @@
 $(document).ready(function () {
 
+    var display = $('.display');
+
+    var sentence = '';
+
+    $('.btn-clear').click(function () {
+
+        sentence = '';
+
+        display.val(0);
+
+    });
+
     $('.btn').click(function () {
 
-        var data = $(this).val();
+        if (sentence.length < 9) {
+
+            if ($.isNumeric($(this).val())) {
+
+                sentence += $(this).val();
+
+                display.val(sentence);
+
+            } else if ($.inArray($(this).val(), ['+', '-', '*', '/', ',']) !== '-1') {
+
+                if ($(this).val() !== sentence.charAt(sentence.length - 1)) {
+                    sentence += $(this).val();
+
+                    display.val(sentence);
+                }
+
+            }
+
+        }
+
+    });
+
+    $('.btn-equal').click(function () {
 
         $.ajax({
 
             url: 'index.php?class=HomeController&method=edit',
             type: 'POST',
-            data: { data: data },
+            data: { sentence: sentence },
             dataType: 'json',
             success: function (response) {
-
-                // var resp = JSON.parse(response);
 
                 console.log(response);
 
